585c3733d24a2b0734f4c86bae9547e2b215e068
Ryan Lirio <ryanlirio2@gmail.com>
2025-08-16 01:10:17 -0300

    Aplicando .gitignore e limpando arquivos temporários

--- GREENHOUSE/Floricultura/Floricultura/Data/DbInitializer.cs
+++ GREENHOUSE/Floricultura/Floricultura/Data/DbInitializer.cs
@@ -1,6 +1,27 @@
-﻿namespace Floricultura.Data
+﻿using Floricultura.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Metrics;
+namespace Floricultura.Data
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
+        public async static void Initialize(PlantsContext context)
+        {
+            await context.Database.EnsureCreatedAsync();
+
+            if (await context.Database.EnsureDeletedAsync())
+                return;
+
+            var plants = new Plant[]
+            {
+                new(){Nome="Lirio", Humidade=23}
+            };
+
+            foreach (Plant p in plants) 
+            { 
+                await context.Plants.AddAsync(p); 
+            }
+            context.SaveChanges();
+        }
     }
 }
--- GREENHOUSE/Floricultura/Floricultura/Models/Plant.cs
+++ GREENHOUSE/Floricultura/Floricultura/Models/Plant.cs
@@ -1,6 +1,12 @@
-﻿namespace Floricultura.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Floricultura.Models
 {
-    public class Plants
+    public class Plant
     {
+        [Key]
+        public string Nome { get; set; }
+        public float? Humidade { get; set; }
+
     }
 }
